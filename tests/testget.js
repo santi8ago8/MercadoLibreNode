fe58220bf574b1aa06617e06c05bf43e75eb8e79
santi8ago8 <santi8ago8@gmail.com>
2013-09-17 16:33:26 -0300

    add examples

--- tests/testget.js
+++ tests/testget.js
@@ -1,14 +1,46 @@
 var meli = require('../index.js');
-
+var sys = require("sys");
 
 var meliObject = new meli.Meli();
-/*
-meliObject.get('sites/MLA/categories', function (a, b, c) {
-    console.log(a, b, c);
-});*/
 
-meliObject.get('users/me', {
-    access_token: 'APP_USR-6092-091714-0290c977f1dd4643e4a4c11ceb831053__H_B__-77169310'
-}, function (a, b, c) {
-    console.log(a, b, c);
+
+var examples = {
+    1: "Get categories from mercado libre argentina",
+    2: "Get users/77169310",
+    3: "Get users?ids=[n,n]"
+};
+function printOptions() {
+    console.log("Ingrese un numero:");
+    for (var i in examples) {
+        console.log(i + ": " + examples[i]);
+
+    }
+}
+printOptions();
+var stdin = process.openStdin();
+stdin.addListener("data", function (d) {
+    var end = d.toString();
+    if (d == 1) {
+        meliObject.get('sites/MLA/categories', function (err, res) {
+            console.log(err, res);
+        });
+    }
+    else if (d == 2) {
+        meliObject.get('users/77169310', function (err, res) {
+            console.log(err, res);
+        });
+    }
+    else if (d == 3) {
+        meliObject.get('users', {
+            ids: [145925943, 145925951]
+        }, function (err, res) {
+            console.log(err, res);
+        });
+    }
+    else if (d == 4) {
+
+    }
 });
+
+
+
