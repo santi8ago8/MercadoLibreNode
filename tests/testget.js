e71075e04c1d56cce6a037d4c47168f73f77e3ff
santi8ago8 <santi8ago8@gmail.com>
2013-09-17 15:28:33 -0300

    fix cb err in get

--- tests/testget.js
+++ tests/testget.js
@@ -2,7 +2,13 @@ var meli = require('../index.js');
 
 
 var meliObject = new meli.Meli();
-
+/*
 meliObject.get('sites/MLA/categories', function (a, b, c) {
+    console.log(a, b, c);
+});*/
+
+meliObject.get('users/me', {
+    access_token: 'APP_USR-6092-091714-0290c977f1dd4643e4a4c11ceb831053__H_B__-77169310'
+}, function (a, b, c) {
     console.log(a, b, c);
 });
